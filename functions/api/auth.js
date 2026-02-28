520ec9b393443e68e0352cf1276f512d3c5bb58a
thezainulabideen <88002620+thezainulabideen@users.noreply.github.com>
2026-02-28 14:01:39 +0500

    Update auth.js

--- functions/api/auth.js
+++ functions/api/auth.js
@@ -4,13 +4,16 @@ export async function onRequest(context) {
   const code = url.searchParams.get('code');
 
   if (!code) {
-    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${env.GITHUB_CLIENT_ID}&scope=repo`;
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${env.GITHUB_CLIENT_ID}&scope=repo,user`;
     return Response.redirect(githubAuthUrl, 302);
   }
 
-  const response = await fetch('https://github.com/login/oauth/access_token', {
+  const tokenResponse = await fetch('https://github.com/login/oauth/access_token', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
     body: JSON.stringify({
       client_id: env.GITHUB_CLIENT_ID,
       client_secret: env.GITHUB_CLIENT_SECRET,
@@ -18,26 +21,34 @@ export async function onRequest(context) {
     })
   });
 
-  const data = await response.json();
+  const data = await tokenResponse.json();
   const token = data.access_token;
+  const provider = 'github';
 
-  const html = `
-    <!DOCTYPE html>
-    <html>
-    <body>
-    <script>
-      const receiveMessage = (message) => {
-        window.opener.postMessage(
-          'authorization:github:success:{"token":"${token}","provider":"github"}',
-          message.origin
-        );
-        window.removeEventListener("message", receiveMessage, false);
-      }
-      window.addEventListener("message", receiveMessage, false);
-      window.opener.postMessage("authorizing:github", "*");
-    </script>
-    </body>
-    </html>`;
+  const html = `<!DOCTYPE html>
+<html>
+<head><title>Authenticating...</title></head>
+<body>
+<p>Authenticating, please wait...</p>
+<script>
+(function() {
+  function receiveMessage(e) {
+    console.log('received message', e);
+    window.opener.postMessage(
+      'authorization:${provider}:success:${JSON.stringify({token, provider})}',
+      e.origin
+    );
+    window.removeEventListener('message', receiveMessage, false);
+  }
+  window.addEventListener('message', receiveMessage, false);
+  console.log('sending authorizing message');
+  window.opener.postMessage('authorizing:${provider}', '*');
+})();
+</script>
+</body>
+</html>`;
 
-  return new Response(html, { headers: { 'Content-Type': 'text/html' } });
+  return new Response(html, {
+    headers: { 'Content-Type': 'text/html' }
+  });
 }
